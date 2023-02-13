cbb2a31aac2749eed30b0b4dbf9cafb142a75ad5
Nenad Laptop <nenadsombor.rs@gmail.com>
2023-02-13 13:05:58 +0100

    import password via getenv

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,12 +123,13 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+email_password = getenv("EmailPassword")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djurdjevicn032@gmail.com'
-EMAIL_HOST_PASSWORD = 'nmgygbeilcwkyzvl'
+EMAIL_HOST_PASSWORD = f"{email_password}"
 
 
